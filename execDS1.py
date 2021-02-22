466040ba62822016bcd02db7bf0893a776e47258
phtorresmo <79370638+phtorresmo@users.noreply.github.com>
2021-02-22 10:32:34 -0300

    Add files via upload

---
+++ execDS1.py
@@ -0,0 +1,9 @@
+idade = float(input("Digite sua idade: "))
+if idade <= 1:
+    print("Iniciante")
+elif idade <= 3:
+    print("Intermediário")
+elif idade <= 6:
+    print("Avançado")
+else:
+    print("Jedi Master")
